258739702669ee3526fafefbbc4e8a899ebd8bc9
Lois Harris <lharris@memberclicks.com>
2017-04-08 22:23:14 -0400

    add form and login function to component

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { Address } from '../../Interface/address';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private address: Address) { }
+  loginForm : FormGroup;
+  constructor(public _fb : FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this._fb.group({
+      username  : ['', Validators.required],
+      password  : ['', Validators.required]
+    })
+  }
+  OnSubmit(value:any){
+
   }
 
 }
